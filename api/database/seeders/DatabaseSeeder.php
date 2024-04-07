83c65648e751a0963da20c86742063e17525e3fa
Hithy <55604744+xHithy@users.noreply.github.com>
2024-04-08 02:33:37 +0300

    Added sub-section category migrations

--- api/database/seeders/DatabaseSeeder.php
+++ api/database/seeders/DatabaseSeeder.php
@@ -2,21 +2,49 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\SubSectionCategory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        // Create base sub-section categories
+        SubSectionCategory::factory()->create([
+            'name' => 'Living Room',
+            'image_path' => '/storage/categories/livingroom.jpg',
+        ]);
+        SubSectionCategory::factory()->create([
+            'name' => 'Bathroom',
+            'image_path' => '/storage/categories/bathroom.jpg',
+        ]);
+        SubSectionCategory::factory()->create([
+            'name' => 'Bedroom',
+            'image_path' => '/storage/categories/bedroom.jpg',
+        ]);
+        SubSectionCategory::factory()->create([
+            'name' => 'Pantry',
+            'image_path' => '/storage/categories/pantry.jpg',
+        ]);
+        SubSectionCategory::factory()->create([
+            'name' => 'Kitchen',
+            'image_path' => '/storage/categories/kitchen.jpg',
+        ]);
+        SubSectionCategory::factory()->create([
+            'name' => 'Garage',
+            'image_path' => '/storage/categories/garage.jpg',
+        ]);
+        SubSectionCategory::factory()->create([
+            'name' => 'Storage',
+            'image_path' => '/storage/categories/storage.jpg',
+        ]);
+        SubSectionCategory::factory()->create([
+            'name' => 'Dining room',
+            'image_path' => '/storage/categories/diningroom.jpg',
+        ]);
+        SubSectionCategory::factory()->create([
+            'name' => 'Office',
+            'image_path' => '/storage/categories/office.jpg',
+        ]);
     }
 }
