3d8d6821c81f8957afcabf90444e27364274656c
Hithy <55604744+xHithy@users.noreply.github.com>
2024-04-17 01:17:26 +0300

    feature/dashboard-base

---
+++ client/src/views/Dashboard/Dashboard.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Container from '../../components/Container';
+import PageTitle from '../../components/PageTitle';
+
+const Dashboard = () => {
+   return (
+      <Container>
+         <PageTitle title='Dashboard' />
+      </Container>
+   );
+};
+
+export default Dashboard;
