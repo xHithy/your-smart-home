e6318228717ddd560f1d896c594f1c97fdf50cd4
Hithy <55604744+xHithy@users.noreply.github.com>
2024-04-24 23:52:05 +0300

    fix/navigation-background

--- client/src/components/NavigationBar/NavigationBar.tsx
+++ client/src/components/NavigationBar/NavigationBar.tsx
@@ -4,7 +4,7 @@ import { NAVIGATION_ICONS } from './NavigationIcon';
 
 const NavigationBar = () => {
    return (
-      <div className='fixed bottom-0 left-0 grid w-full grid-cols-3 items-center gap-2 px-5 pb-3 pt-2 md:static md:px-0 md:pt-0'>
+      <div className='fixed bottom-0 left-0 z-10 grid w-full grid-cols-3 items-center gap-2 bg-gray-100 px-5 pb-3 pt-2 md:static md:px-0 md:pt-0'>
          <NavigationItem
             name='Dashboard'
             route='/dashboard'
