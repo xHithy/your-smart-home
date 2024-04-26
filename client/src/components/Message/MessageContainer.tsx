770be8a6e74bf180a1b9d2b0a7765c977af4cce2
Hithy <55604744+xHithy@users.noreply.github.com>
2024-04-26 03:25:24 +0300

    feature/message-container

---
+++ client/src/components/Message/MessageContainer.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useMessages } from '../../providers/MessageContext';
+import SingleMessage from './SingleMessage';
+
+const MessageContainer = () => {
+   const { messages, removeMessage } = useMessages();
+
+   return (
+      <div className='fixed right-0 top-0 z-50 flex h-auto flex-col-reverse pt-5'>
+         {messages.map((msg, index) => (
+            <SingleMessage
+               key={index}
+               message={msg.message}
+               type={msg.type}
+               onRemove={() => removeMessage(msg.id)}
+            />
+         ))}
+      </div>
+   );
+};
+
+export default MessageContainer;
