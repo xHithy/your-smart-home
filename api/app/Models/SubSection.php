da57cf90ddacede9216a8d30aecd86f3e75dffa5
Hithy <55604744+xHithy@users.noreply.github.com>
2024-05-14 01:44:38 +0300

    Updated ide-helper

--- api/app/Models/SubSection.php
+++ api/app/Models/SubSection.php
@@ -12,6 +12,18 @@ use Illuminate\Database\Eloquent\Model;
  * @method static \Illuminate\Database\Eloquent\Builder|SubSection newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|SubSection newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|SubSection query()
+ * @property int $id
+ * @property string $name
+ * @property int $section_id
+ * @property int $category_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|SubSection whereCategoryId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|SubSection whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|SubSection whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|SubSection whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|SubSection whereSectionId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|SubSection whereUpdatedAt($value)
  * @mixin Eloquent
  */
 class SubSection extends Model
