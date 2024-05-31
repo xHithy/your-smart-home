560abb47f794209f813f5ec4d27809f1bcd9302f
Hithy <55604744+xHithy@users.noreply.github.com>
2024-05-31 17:53:28 +0300

    Created Dashboard page

--- client/src/views/Dashboard/Dashboard.tsx
+++ client/src/views/Dashboard/Dashboard.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import Container from '../../components/Container';
 import PageTitle from '../../components/PageTitle';
+import NavigationBar from '../../components/NavigationBar/NavigationBar';
+import Pinned from './Pinned/Pinned';
 
 const Dashboard = () => {
    return (
       <Container>
-         <PageTitle title='Dashboard' />
+         <NavigationBar />
+         <div className='sticky top-0 z-10 flex w-full items-center justify-between space-x-1 bg-gray-100 py-3'>
+            <PageTitle title='Dashboard' />
+         </div>
+         <div className='flex flex-col'>
+            <Pinned />
+         </div>
       </Container>
    );
 };
